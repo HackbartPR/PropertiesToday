fa20a53b3b24b4213514c114511440968a278bc8
HackbartPR <cgharckbart@gmail.com>
2023-07-15 15:44:16 -0300

    Primeiro Mediator Request

---
+++ Application/MappingProfiles/Mapping.cs
@@ -0,0 +1,13 @@
+﻿using Application.Models;
+using AutoMapper;
+using Domain;
+
+namespace Application.MappingProfiles;
+
+public class Mapping : Profile
+{
+    public Mapping()
+    {
+        CreateMap<NewPropertyRequest, Property>();
+    }
+}
---
+++ Application/Repositories/IPropertyRepo.cs
@@ -0,0 +1,12 @@
+﻿using Domain;
+
+namespace Application.Repositories;
+
+public interface IPropertyRepo
+{
+    Task AddNewAsync(Property property);
+    Task UpdateAsync(Property property);
+    Task DeleteAsync(int id);
+    Task<Property> GetByIdAsync(int id);
+    Task<List<Property>> GetAllAsync();
+}
