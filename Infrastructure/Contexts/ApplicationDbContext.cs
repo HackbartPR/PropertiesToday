3d6b26326338b27389847374055862649d01617a
HackbartPR <cgharckbart@gmail.com>
2023-07-15 16:16:07 -0300

    Criando um Repository para o Property

---
+++ Infrastructure/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Contexts;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Property> Properties => Set<Property>();
+    public DbSet<Image> Images => Set<Image>();
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    { 
+    }
+}
---
+++ Infrastructure/Repositories/PropertyRepo.cs
@@ -0,0 +1,41 @@
+﻿using Application.Repositories;
+using Domain;
+using Infrastructure.Contexts;
+
+namespace Infrastructure.Repositories;
+
+public class PropertyRepo : IPropertyRepo
+{
+    private readonly ApplicationDbContext _context;
+
+    public PropertyRepo(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddNewAsync(Property property)
+    {
+        await _context.Properties.AddAsync(property);
+        await _context.SaveChangesAsync();
+    }
+
+    public Task DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Property>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Property> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(Property property)
+    {
+        throw new NotImplementedException();
+    }
+}
