8c5f12df899b1862600cb6775b07884419e2e629
HackbartPR <cgharckbart@gmail.com>
2023-07-18 19:51:04 -0300

    Criando Repositorio para Image

---
+++ Application/Repositories/IImageRepo.cs
@@ -0,0 +1,12 @@
+﻿using Domain;
+
+namespace Application.Repositories;
+
+public interface IImageRepo
+{
+    public Task AddNewAsync(Image image);
+    public Task DeleteAsync(Image image);
+    public Task UpdateAsync(Image image);
+    public Task<Image> GetByAsync(int id);
+    public Task<List<Image>> GetAllAsync();
+}
---
+++ Infrastructure/Repositories/ImageRepo.cs
@@ -0,0 +1,44 @@
+﻿using Application.Repositories;
+using Domain;
+using Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+public class ImageRepo : IImageRepo
+{
+    private readonly ApplicationDbContext _context;
+
+    public ImageRepo(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddNewAsync(Image image)
+    {
+        await _context.Images.AddAsync(image);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(Image image)
+    {
+        _context.Images.Remove(image);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<List<Image>> GetAllAsync()
+    {
+        return await _context.Images.ToListAsync();
+    }
+
+    public async Task<Image> GetByAsync(int id)
+    {
+        return await _context.Images.FirstOrDefaultAsync(x => x.Id == id);
+    }
+
+    public async Task UpdateAsync(Image image)
+    {
+        _context.Images.Update(image);
+        await _context.SaveChangesAsync();
+    }
+}
